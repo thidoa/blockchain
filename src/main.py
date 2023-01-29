006a54ff006a0d11401765321ab500fd2f3eb9b7
devid <nnick4673@gmail.com>
2023-01-29 19:54:41 -0300

    Conteúdos dos blocos alterados

--- src/main.py
+++ src/main.py
@@ -3,8 +3,8 @@ from blockchain import Blockchain
 if __name__ == '__main__':
     blockchain = Blockchain()
 
-    blockchain.add_new_block('Block First!')
-    blockchain.add_new_block('Blockchain is very nice!')
-    blockchain.add_new_block('Speak some languages ?')
+    blockchain.add_new_block('Primeiro bloco')
+    blockchain.add_new_block('Segundo bloco')
+    blockchain.add_new_block('Terceiro bloco')
 
     print(blockchain.get_all_blocks())
